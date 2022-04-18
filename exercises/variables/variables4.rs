2a12c8b62741c8295b87c5b461eed5b944667a5b
timjhh <timjharrold@gmail.com>
2022-04-17 21:43:38 -0400

    Working through practices again

--- exercises/variables/variables4.rs
+++ exercises/variables/variables4.rs
@@ -1,9 +1,8 @@
 // variables4.rs
 // Make me compile! Execute the command `rustlings hint variables4` if you want a hint :)
 
-// I AM NOT DONE
 
 fn main() {
-    let x: i32;
-    println!("Number {}", x);
+    let x: i32 = 10;
+    println!("Number {0}", x);
 }
